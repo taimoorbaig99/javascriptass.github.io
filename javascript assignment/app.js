fcf5ff6420aff7eecc6919fe4ca075120e2fdd20
taimoorbaig99 <muhammadtaimoorbaig64@gmail.com>
2022-10-31 00:49:18 +0400

    Javascript assignment

---
+++ javascript assignment/app.js
@@ -0,0 +1,54 @@
+alert("Hello World");
+window.alert("Hey");
+
+
+
+var a=(30);
+alert(a);
+
+
+var b=(20);
+alert(b);
+
+
+var a=(30);
+var b=(20);
+
+
+alert(a+b);
+alert(a*b);
+alert(a-b);
+alert(a/b);
+
+var a=20;
+var b=30;
+
+a++;
+alert(a);
+
+var a=3;
+var b=4;
+alert(c=a+b);
+
+
+
+var a=70;
+var b=80;
+a++;
+b++;
+console.log(a);
+console.log(b);
+
+
+var a=20;
+var b=++a;
+alert(b);
+console.log(b)
+
+
+
+
+
+
+
+
